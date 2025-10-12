d4bf5e699b457aee51a71e7da7bb4e36e6494a6e
Chris <chrisbreuer93@gmail.com>
2025-10-11 17:14:37 -0700

    chore: wip
    chore: wip

--- build.ts
+++ build.ts
@@ -1,6 +1,5 @@
 import process from 'node:process'
 import { dts } from 'bun-plugin-dtsx'
-import { buildDocs } from './bin/cli'
 
 console.log('Building library...')
 
@@ -22,17 +21,6 @@ if (!result.success) {
   for (const log of result.logs) {
     console.error(log)
   }
-  process.exit(1)
-}
-
-console.log('Building documentation...')
-const docsSuccess = await buildDocs({
-  outdir: './dist/docs',
-  verbose: true,
-})
-
-if (!docsSuccess) {
-  console.error('Documentation build failed')
   process.exit(1)
 }
 
